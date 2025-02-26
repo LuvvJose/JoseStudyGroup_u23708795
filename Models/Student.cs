9de0c53d280d082e813393c5e68be5229226ffae
Jose <Jose@LAPTOP-09EC4VSG>
2025-02-26 12:27:51 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JoseStudyGroup.Models
+{
+	public class Student
+	{
+        [Display(Name = "STUDENT NUMBER")]
+        public int StudentNumber { get; set; }
+
+        [Display(Name = "STUDENT NUMBER")]
+        public int StudentNumber { get; set; }
+
+        [Display(Name = "STUDENT NUMBER")]
+        public int StudentNumber { get; set; }
+
+    }
+}
